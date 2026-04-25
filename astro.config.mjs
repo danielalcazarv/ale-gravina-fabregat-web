3541804812074d319d52ba4812d33adfe6551a28
Daniel Alcázar <danielalcazar54@gmail.com>
2026-04-25 09:58:05 -0300

    mod| responsive header hero service contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static'
+    output: 'static',
+    devToolbar: {
+        enabled: false
+    }
 });
+
